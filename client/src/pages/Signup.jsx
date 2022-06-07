1da4cb46d21cd67e1c1d0129590b9512ef5fbd2b
Chance Supcoe <chancesupcoe@gmail.com>
2022-06-07 19:09:41 -0400

    restoring progress

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { registerRoute } from "../utils/Routes";
+
+export default function Signup() {
+    return (
+        <div>
+            <p>Test Signup Page</p>
+        </div>
+    )
+};
+
