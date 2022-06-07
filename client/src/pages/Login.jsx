e33491269734668743c0fe345b8b678fa41ea40e
Chance Supcoe <chancesupcoe@gmail.com>
2022-06-07 19:40:03 -0400

    login form functions

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,8 +6,57 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { loginRoute } from "../utils/Routes";
 
-
 export default function Login() {
+
+    const navigate = useNavigate();
+    const [values, setValues] = useState({username: '', password: ''});
+
+    useEffect(() => {
+        if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+          navigate("/");
+        }
+      }, []);
+
+      const handleChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+      };
+    
+      const validateForm = () => {
+        const { username, password } = values;
+        if (username === "") {
+          console.error("Email and Password Required for Login.");
+          return false;
+        } else if (password === "") {
+          console.error("Email and Password Required for Login.");
+          return false;
+        }
+        return true;
+      };
+
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (validateForm()) {
+          const { username, password } = values;
+          const { data } = await axios.post(loginRoute, {
+            username,
+            password,
+          });
+          if (data.status === false) {
+            console.error(data.msg);
+          }
+          if (data.status === true) {
+            localStorage.setItem(
+              process.env.REACT_APP_LOCALHOST_KEY,
+              JSON.stringify(data.user)
+            );
+    
+            navigate("/");
+          }
+        }
+      };
+    
+    
+
     return (
         <div>
             <p>Test Login Page</p>
