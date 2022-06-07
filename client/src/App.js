9524dab214cbde2c35c58845b41b3eeb1d46dcb9
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-06-07 19:28:52 -0400

    minor updates

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/avatar" element={<Avatar />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Avatar" element={<Avatar />} />
         <Route path="/" element={<Chat />} />
       </Routes>
     </BrowserRouter>
