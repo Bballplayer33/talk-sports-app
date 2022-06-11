dd0f534a33a0ad549e8bbd026c36790c62659039
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-06-11 12:35:54 -0400

    cleaned up signin and login page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import"../styles/Navbar.css"
 export default function Navbar(){
     return(
         <nav>
-            <h1>Test Nav Bar</h1>
+            <h1>QUOTE OF THE DAY: “Winners never quit and quitters never win.” —Vince Lombardi </h1>
             <ul>
                 <li><a href="/login"></a>Login</li>
                 <li><a href="/Signup"></a>Signup</li>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -73,7 +73,7 @@ export default function Signup() {
 <SignupContainer>
     <form action="" onSubmit={(event) => handleSubmit(event)}>
         <div className="header">
-            <h1>Talk Sports</h1>
+          <h1>TALK SPORTS</h1>
         </div>
         <input
         type= 'text'
