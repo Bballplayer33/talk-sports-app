acda71122dd521fc8b9562942f405dae4f1b1789
Chance Supcoe <chancesupcoe@gmail.com>
2022-06-11 12:01:45 -0400

    settings

---
+++ client/src/pages/Settings.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+export default function Settings() {
+    const navigate = useNavigate();
+    const socket = useRef;
+    const [currentUser, setCurrentUser] = useState(undefined);
+
+    useEffect(async () => {
+        if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+          navigate("/login");
+        } else {
+          setCurrentUser(
+            await JSON.parse(
+              localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+            )
+          );
+        }
+      }, []);
+
+
+
+    return (
+        <>
+            <SettingsContainer>
+               
+            </SettingsContainer>
+        </>
+    )
+}
+
+const SettingsContainer = styled.div `
+    height:100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+
+`;
