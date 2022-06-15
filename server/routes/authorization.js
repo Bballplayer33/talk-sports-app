8309e7e84c3338ea3b13649148db30d99070b736
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-06-15 00:37:00 -0400

    avatar route update

--- server/routes/authorization.js
+++ server/routes/authorization.js
@@ -11,7 +11,7 @@ const router = require("express").Router();
 router.post("/login", login);
 router.post("/signup", signup);
 router.get("/allUsers/:id", getAllUsers);
-router.post("/setavatar/:id", setAvatar);
+router.post("/avatar/:id", setAvatar);
 router.get("/logout/:id", logOut);
 
 module.exports = router;
